0d3d46635078511b8944597412020567761c6429
eistrej <reistvan@gmail.com>
2021-12-06 14:42:18 +0100

    Implement second part of the 6th day solution
    Code was refactored to make it quicker.

--- 2021/d06_lanternfish/d06_lanternfish.py
+++ 2021/d06_lanternfish/d06_lanternfish.py
@@ -4,17 +4,24 @@ from util import read_file
 class LanternFishes:
     def __init__(self, beginning_fishes):
         self._fishes = beginning_fishes
+        self._counters = [
+            [beginning_fishes.count(index) for index in range(6)]
+        ]
 
     def get_number_of_fishes(self):
-        return len(self._fishes)
+        return sum(self._counters[-1])
 
     def iterate_one_day(self):
-        for index in range(len(self._fishes)):
-            if self._fishes[index] == 0:
-                self._fishes[index] = 6
-                self._fishes.append(8)
+        number_of_iterations = len(self._counters)
+        new_iteration = list()
+        for index in range(6):
+            if index + 1 == number_of_iterations:
+                new_iteration.append(self._counters[-1][index] * 2)
+            elif number_of_iterations < 9:
+                new_iteration.append(self._counters[-1][index])
             else:
-                self._fishes[index] -= 1
+                new_iteration.append(self._counters[-7][index] + self._counters[-9][index])
+        self._counters.append(new_iteration)
 
     def iterate_several_days(self, number_of_iterations):
         for _ in range(number_of_iterations):
@@ -28,3 +35,5 @@ def solve():
     lantern_fishes.iterate_several_days(80)
     print("Day 06")
     print(f"  - Part 1: {lantern_fishes.get_number_of_fishes()}")
+    lantern_fishes.iterate_several_days(176)
+    print(f"  - Part 2: {lantern_fishes.get_number_of_fishes()}")
--- 2021/d06_lanternfish/d06_lanternfish_test.py
+++ 2021/d06_lanternfish/d06_lanternfish_test.py
@@ -12,7 +12,6 @@ class LanternFishesTestCase(unittest.TestCase):
         testcases = [
             ([3, 4, 3, 1, 2], 5),
             ([2, 3, 2, 0, 1], 6),
-            ([0, 1, 0, 5, 6, 7, 8], 9)
         ]
         for fishes, expected_number_of_fishes in testcases:
             lantern_fishes = d06.LanternFishes(fishes)
@@ -31,20 +30,8 @@ class LanternFishesTestCase(unittest.TestCase):
         lantern_fishes = d06.LanternFishes([3, 4, 3, 1, 2])
         lantern_fishes.iterate_several_days(80)
         self.assertEqual(5934, lantern_fishes.get_number_of_fishes())
-
-    def test_check_fishes_after_iteration(self):
-        lantern_fishes = d06.LanternFishes([3, 4, 3, 1, 2])
-        self.assertListEqual([3, 4, 3, 1, 2], lantern_fishes._fishes)
-        lantern_fishes.iterate_one_day()
-        self.assertListEqual([2, 3, 2, 0, 1], lantern_fishes._fishes)  # day 1
-        lantern_fishes.iterate_one_day()
-        self.assertListEqual([1, 2, 1, 6, 0, 8], lantern_fishes._fishes)  # day 2
-        lantern_fishes.iterate_one_day()
-        self.assertListEqual([0, 1, 0, 5, 6, 7, 8], lantern_fishes._fishes)  # day 3
-        lantern_fishes.iterate_one_day()
-        self.assertListEqual([6, 0, 6, 4, 5, 6, 7, 8, 8], lantern_fishes._fishes)  # day 4
-        lantern_fishes.iterate_one_day()
-        self.assertListEqual([5, 6, 5, 3, 4, 5, 6, 7, 7, 8], lantern_fishes._fishes)  # day 5
+        lantern_fishes.iterate_several_days(176)
+        self.assertEqual(26984457539, lantern_fishes.get_number_of_fishes())
 
 
 if __name__ == '__main__':
