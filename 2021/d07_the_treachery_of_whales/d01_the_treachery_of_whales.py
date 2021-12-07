909849ccc72d7774627b2a375d3447e89bfbc05a
eistrej <reistvan@gmail.com>
2021-12-07 10:13:40 +0100

    Implement the 2nd part of the 7th day

--- 2021/d07_the_treachery_of_whales/d01_the_treachery_of_whales.py
+++ 2021/d07_the_treachery_of_whales/d01_the_treachery_of_whales.py
@@ -23,9 +23,34 @@ def count_min_fuel_for_crab_moving(crabs):
     return minimal_movement
 
 
+def fuel_consumption(distance):
+    delta = 1 if distance > 0 else -1
+    return (distance + delta) * distance // 2
+
+
+def count_min_revised_fuel_consumption(crabs):
+    count_of_crabs = dict()
+    minimum_position_of_crabs = min(crabs)
+    maximum_position_of_crabs = max(crabs) + 1
+    for number_of_crabs in range(minimum_position_of_crabs, maximum_position_of_crabs):
+        count_of_crabs[number_of_crabs] = crabs.count(number_of_crabs)
+    minimal_fuel_consumption = 999999999999999
+    for index in range(minimum_position_of_crabs, maximum_position_of_crabs):
+        sum_fuel_consumption = 0
+        for crab_index, count in count_of_crabs.items():
+            sum_fuel_consumption += fuel_consumption(index - crab_index) * count
+        if sum_fuel_consumption < minimal_fuel_consumption:
+            minimal_fuel_consumption = sum_fuel_consumption
+        else:
+            break
+    return minimal_fuel_consumption
+
+
 def solve():
     lines_of_file = read_file("d07_the_treachery_of_whales/input.txt")
     crabs_positions = [int(crab_position.strip()) for crab_position in lines_of_file[0].split(",")]
     minimal_movement = count_min_fuel_for_crab_moving(crabs_positions)
+    revised_minimal_movement = count_min_revised_fuel_consumption(crabs_positions)
     print("Day 7:")
     print(f"  - Part 1: {minimal_movement}")
+    print(f"  - Part 2: {revised_minimal_movement}")
--- 2021/d07_the_treachery_of_whales/d01_the_treachery_of_whales_test.py
+++ 2021/d07_the_treachery_of_whales/d01_the_treachery_of_whales_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from d07_the_treachery_of_whales.d01_the_treachery_of_whales import count_min_fuel_for_crab_moving
+from d07_the_treachery_of_whales.d01_the_treachery_of_whales import count_min_fuel_for_crab_moving, \
+    count_min_revised_fuel_consumption
 
 
 class TheTreacheryOfWhalesTestCase(unittest.TestCase):
@@ -8,6 +9,11 @@ class TheTreacheryOfWhalesTestCase(unittest.TestCase):
         crabs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
         fuel = count_min_fuel_for_crab_moving(crabs)
         self.assertEqual(37, fuel)
+
+    def test_revised_minimal_fuel_consumption(self):
+        crabs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+        fuel = count_min_revised_fuel_consumption(crabs)
+        self.assertEqual(168, fuel)
 
 
 if __name__ == '__main__':
