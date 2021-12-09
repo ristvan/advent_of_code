4eec4ea85edeb7e2c0582547dab96272ef940002
eistrej <reistvan@gmail.com>
2021-12-09 13:09:14 +0100

    Implement the 2nd part of 9th day solution

--- 2021/d09_smoke_basin/d09_smoke_basin.py
+++ 2021/d09_smoke_basin/d09_smoke_basin.py
@@ -1,3 +1,4 @@
+from math import prod
 from util import read_file
 
 NEIGHBOURS = [
@@ -6,14 +7,20 @@ NEIGHBOURS = [
 
 
 def is_all_neighbours_are_higher(height_map, row_index, column_index):
+    valid_neighbours = get_valid_neighbours(height_map, row_index, column_index)
+    higher_neighbours = [(neighbour_row, neighbour_column) for neighbour_row, neighbour_column in valid_neighbours if
+                         height_map[neighbour_row][neighbour_column] > height_map[row_index][column_index]]
+    return len(valid_neighbours) == len(higher_neighbours)
+
+
+def get_valid_neighbours(height_map, row_index, column_index):
+    valid_neighbours = list()
     for neighbour_row_delta, neighbour_column_delta in NEIGHBOURS:
         neighbour_row = neighbour_row_delta + row_index
         neighbour_column = neighbour_column_delta + column_index
-        if is_valid_neighbour(height_map, neighbour_row, neighbour_column) \
-                and height_map[neighbour_row][neighbour_column] <= height_map[row_index][column_index]:
-           return False
-
-    return True
+        if is_valid_neighbour(height_map, neighbour_row, neighbour_column):
+            valid_neighbours.append((neighbour_row, neighbour_column))
+    return valid_neighbours
 
 
 def is_valid_neighbour(height_map, neighbour_row, neighbour_column):
@@ -37,11 +44,33 @@ def find_low_points(height_map, positions_of_low_points):
     return low_points
 
 
+def count_size_of_basin(height_map, position):
+    points_of_basin = set()
+    next_positions_check = [position]
+    while len(next_positions_check):
+        row, column = next_positions_check.pop(0)
+        points_of_basin.add((row, column))
+        for neighbour_row, neighbour_column in get_valid_neighbours(height_map, row, column):
+            if 9 > height_map[neighbour_row][neighbour_column] > height_map[row][column] \
+                    and (neighbour_row, neighbour_column) not in points_of_basin:
+                next_positions_check.append((neighbour_row, neighbour_column))
+
+    return len(points_of_basin)
+
+
+def get_all_the_size_of_basins(height_map, low_points):
+    return [count_size_of_basin(height_map, low_point) for low_point in low_points]
+
+
 def solve():
     lines_of_file = read_file("d09_smoke_basin/input.txt")
     sea_floor_height_map = [
         [int(height) for height in line_of_file] for line_of_file in lines_of_file
     ]
-    low_points = find_low_points(sea_floor_height_map, find_positions_of_low_points(sea_floor_height_map))
-    print("Day09")
+    positions_of_low_points = find_positions_of_low_points(sea_floor_height_map)
+    low_points = find_low_points(sea_floor_height_map, positions_of_low_points)
+    size_of_basins = get_all_the_size_of_basins(sea_floor_height_map, positions_of_low_points)
+    multiplication_the_three_largest_basins = prod(list(sorted(size_of_basins, reverse=True))[0:3])
+    print("Day 09")
     print(f"  - Part 1: {sum(low_points) + len(low_points)}")
+    print(f"  - Part 2: {multiplication_the_three_largest_basins}")
--- 2021/d09_smoke_basin/d09_smoke_basin_test.py
+++ 2021/d09_smoke_basin/d09_smoke_basin_test.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 
 import d09_smoke_basin as d09
@@ -27,6 +28,31 @@ class SmokeBasinTestCase(unittest.TestCase):
         expected_low_points = [1, 0, 5, 5]
         actual_positions_of_low_points = d09.find_positions_of_low_points(height_map)
         self.assertListEqual(expected_low_points, d09.find_low_points(height_map, actual_positions_of_low_points))
+
+    def test_count_size_of_basin(self):
+        height_map = [
+            [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+            [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+            [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+            [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+            [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]
+        ]
+        self.assertEqual(3, d09.count_size_of_basin(height_map, (0, 1)))
+        self.assertEqual(9, d09.count_size_of_basin(height_map, (0, 9)))
+        self.assertEqual(14, d09.count_size_of_basin(height_map, (2, 2)))
+        self.assertEqual(9, d09.count_size_of_basin(height_map, (4, 6)))
+
+    def test_get_all_the_size_of_basins(self):
+        height_map = [
+            [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+            [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+            [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+            [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+            [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]
+        ]
+        low_points = [(0, 1), (0, 9), (2, 2), (4, 6)]
+        self.assertListEqual([3, 9, 14, 9], d09.get_all_the_size_of_basins(height_map, low_points))
+        self.assertEqual(1134, math.prod(list(sorted([3, 9, 14, 9], reverse=True))[0:3]))
 
 
 if __name__ == '__main__':
